bb48483c5b8302b52013b90629956aa7c309bb8b
Tien LE <letien.dev@gmail.com>
2020-02-09 07:43:09 +0100

    Added Environment Variables - ubuntu

---
+++ environment_variables_pyspark.sh
@@ -0,0 +1,11 @@
+export SPARK_HOME="/home/tienhuong/dev/work/tool/spark-2.4.4-bin-hadoop2.7"
+
+export PATH=$SPARK_HOME:$PATH
+
+export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
+
+export PYSPARK_DRIVER_PYTHON="jupyter"
+
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+
+export PYSPARK_PYTHON="/home/tienhuong/venv/bin/./python3"
