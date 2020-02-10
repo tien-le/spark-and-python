139c4ed2ecbabb4367a386796003d320c8268bd7
Tien LE <letien.dev@gmail.com>
2020-02-10 06:37:03 +0100

    Add JAVA HOME

--- environment_variables_pyspark.sh
+++ environment_variables_pyspark.sh
@@ -1,7 +1,5 @@
 export SPARK_HOME="/home/tienhuong/dev/work/tool/spark-2.4.4-bin-hadoop2.7"
 
-export PATH=$SPARK_HOME:$PATH
-
 export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
 
 export PYSPARK_DRIVER_PYTHON="jupyter"
@@ -9,3 +7,8 @@ export PYSPARK_DRIVER_PYTHON="jupyter"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
 
 export PYSPARK_PYTHON="/home/tienhuong/venv/bin/./python3"
+
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+export PATH=$SPARK_HOME:$JAVA_HOME/bin:$PATH
+
